4e700ce624b82683c129535d5dfa4ce6d065b163
Tyler Collier <tyler.collier+github@devmounta.in>
2017-09-03 13:38:39 -0700

    Add logged-in-as solution

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loginName: ''
+    };
+
+    this.updateLoginName = this.updateLoginName.bind(this);
+  }
+
+  updateLoginName(name) {
+    this.setState({ loginName: name })
+  }
 
   render() {
-    const { loggedInAs, isCreditCardHolder } = this.props;
+    const { loggedInAs, isCreditCardHolder, setLoggedInAs } = this.props;
 
     return (
       <div className="login">
         {!loggedInAs && <div>
-          Sign in as <input /> <button className="button">Go</button>
+          Sign in as <input onChange={(e) => this.updateLoginName(e.target.value)} /> <button className="button" onClick={() => setLoggedInAs(this.state.loginName)}>Go</button>
         </div>}
         {loggedInAs && <div>
           Welcome, {loggedInAs}! {isCreditCardHolder && "(CC holder)"}
